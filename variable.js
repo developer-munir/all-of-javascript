30fa06a631b408679e8b7ce3de475c96a11dc8ba
Munir Hossain Juwel <munirhossainjuwel@gmail.com>
2025-04-18 19:55:17 +0800

    variables

---
+++ variable.js
@@ -0,0 +1,25 @@
+// console.log(NAME)  ----  wrong
+
+const NAME = 'Juwel';
+
+// console.log(NAME) ---- Right 
+
+
+// console.log(a)  ---- wrong
+let a = 'AAAA';
+
+// console.log(a)--- right
+
+// console.log(b) -- using var both are right
+
+var b = 'BBBB';
+// console.log(b)
+
+
+
+// const lima ;  if using const must be initialized value;
+// let lima; var let using this way no problem;
+// console.log(lima) wrong way
+lima = 'Best friend';
+// console.log(lima); right way
+
