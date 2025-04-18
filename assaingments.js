3d77d7d4165b1b2cb90037ed561ce710234aa412
Munir Hossain Juwel <munirhossainjuwel@gmail.com>
2025-04-18 22:27:55 +0800

    how to work assaingment operators

--- assaingments.js
+++ assaingments.js
@@ -1,2 +1,18 @@
 // + , - , * , / , %
 // += , -= , *= , /= , %= , **=
+
+// let x = 3;
+// let y = 4;
+// // console.log(x+y)
+// x += y;
+// console.log(x)
+
+// let a = 3 ;
+// let b = 3 ;
+
+// let multi = (a**b) ----- quare 
+// console.log(multi)
+
+let c = 3;
+c**=2;
+console.log(c)
