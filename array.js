1c16b9e1d500f7f6156e676511985ed838c77797
Munir Hossain Juwel <munirhossainjuwel@gmail.com>
2025-04-19 12:59:19 +0800

    array access

---
+++ array.js
@@ -0,0 +1,12 @@
+// array , array methods , array sort , array search , array lteration
+
+// array 
+const names = ['juwel','akash'];
+// console.log(names[1])
+names[1]='hridoy';
+// console.log(names)
+// console.log(names.length)
+
+// access the last element of a array
+let lastElement = names[names.length-1]
+console.log(lastElement)
