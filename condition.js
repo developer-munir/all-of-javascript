fb14881ebdcd4fd692a987d546d2b9bcba251d13
Munir Hossain Juwel <munirhossainjuwel@gmail.com>
2025-04-19 11:41:00 +0800

    conditonal statements

---
+++ condition.js
@@ -0,0 +1,13 @@
+// if , else , else if , switch ( case , default , break)
+let juwelAge = 27;
+let munirAge = 20;
+if(juwelAge===munirAge){
+    console.log('both are same age');
+}else if(juwelAge>=munirAge)
+{
+    console.log('juwel is bigger than munir')
+}else{
+    console.log('both condition are not true')
+}
+
+
